f1aeb07a38f3d7fd43ff3f26298b6031b3c5a9d5
Xavi <xaviregnier@gmail.com>
2020-07-04 15:03:29 +0200

    (feat/JWT): auth0 dependency imported and JWTAutheticationFilter.java class done

--- src/main/java/com/patrimoine/website/core/security/WebSecurityConfig.java
+++ src/main/java/com/patrimoine/website/core/security/WebSecurityConfig.java
@@ -3,6 +3,4 @@ package com.patrimoine.website.core.security;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Ove
 }
