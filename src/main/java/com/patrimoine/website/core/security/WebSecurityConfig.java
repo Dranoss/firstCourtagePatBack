c5050201a79ed96c56832267a0c61b298c74d020
Xavi <xaviregnier@gmail.com>
2020-07-04 13:57:36 +0200

    (feat/jwt): Websecurity config created & findByEmail added to UserRepository

---
+++ src/main/java/com/patrimoine/website/core/security/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.patrimoine.website.core.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Ove
+}
